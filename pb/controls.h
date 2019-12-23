7f9550377a86a53670e902312e08feaa73a23a88
Mark Lentczner <markl@glyphic.com>
2019-12-23 14:19:27 -0800

    move controls into controls.h/.cpp

---
+++ pb/controls.h
@@ -0,0 +1,15 @@
+#ifndef _INCLUDE_CONTROLS_H_
+#define _INCLUDE_CONTROLS_H_
+
+#include "ui_input.h"
+extern IdleTimeout selectionTimeout;
+extern IdleTimeout dimTimeout;
+
+extern Encoder encoder;
+extern Button encoderButton;
+
+extern Button oledButtonA;
+extern Button oledButtonB;
+extern Button oledButtonC;
+
+#endif // _INCLUDE_CONTROLS_H_
--- pb/pb.ino
+++ pb/pb.ino
@@ -5,52 +5,12 @@
 #endif
 
 #include "clock.h"
+#include "controls.h"
 #include "display.h"
 #include "layout.h"
 #include "state.h"
 #include "timer_hw.h"
-#include "ui_input.h"
 
-
-// OLED FeatherWing buttons map to different pins depending on board:
-#if defined(ESP8266)
-  #define BUTTON_A  0
-  #define BUTTON_B 16
-  #define BUTTON_C  2
-#elif defined(ESP32)
-  #define BUTTON_A 15
-  #define BUTTON_B 32
-  #define BUTTON_C 14
-#elif defined(ARDUINO_STM32_FEATHER)
-  #define BUTTON_A PA15
-  #define BUTTON_B PC7
-  #define BUTTON_C PC5
-#elif defined(TEENSYDUINO)
-  #define BUTTON_A  4
-  #define BUTTON_B  3
-  #define BUTTON_C  8
-#elif defined(ARDUINO_FEATHER52832)
-  #define BUTTON_A 31
-  #define BUTTON_B 30
-  #define BUTTON_C 27
-#else // 32u4, M0, M4, nrf52840 and 328p
-  #define BUTTON_A  9
-  #define BUTTON_B  6
-  #define BUTTON_C  5
-#endif
-
-#define ENCODER_SW  10
-#define ENCODER_A   11
-#define ENCODER_B   12
-
-
-IdleTimeout selectionTimeout(2000);
-IdleTimeout dimTimeout(5000);
-Encoder encoder(ENCODER_A, ENCODER_B);
-Button encoderButton(ENCODER_SW);
-Button oledButtonA(BUTTON_A);
-Button oledButtonB(BUTTON_B);
-Button oledButtonC(BUTTON_C);
 
 #ifdef ZERO_REGS_H
 ZeroRegOptions zeroOpts = { Serial, true };
