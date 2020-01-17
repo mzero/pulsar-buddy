660e47e73b9389656e433b2f7b9910ed09fc013d
Mark Lentczner <markl@glyphic.com>
2020-01-17 15:52:24 -0800

    v103 release

--- pb/version.h
+++ pb/version.h
@@ -1,9 +1,16 @@
 #ifndef _INCLUDE_VERSION_H_
 #define _INCLUDE_VERSION_H_
 
-#define PB_VERSION  2
-  /* 2020-01-17
+#define PB_VERSION 103
+/* 2020-01-17
+  - extended BPM range option
+  - improve long press behavior
+*/
 
-  */
+// PB_VERSION 102
+/* 2020-01-17
+  - first version with a version
+*/
+
 
 #endif // _INCLUDE_VERSION_H_
